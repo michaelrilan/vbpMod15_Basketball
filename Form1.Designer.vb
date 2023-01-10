c5a2702f29ee48e7531166183c9762c397afadd5
michaelrilan <micorilan1999@gmail.com>
2023-01-11 02:01:44 +0800

    Add project files.

---
+++ Form1.Designer.vb
@@ -0,0 +1,319 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.name_p1_blue = New System.Windows.Forms.TextBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.num_p1_blue = New System.Windows.Forms.TextBox()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.num_p2_blue = New System.Windows.Forms.TextBox()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.name_p2_blue = New System.Windows.Forms.TextBox()
+        Me.Label9 = New System.Windows.Forms.Label()
+        Me.num_p2_red = New System.Windows.Forms.TextBox()
+        Me.Label10 = New System.Windows.Forms.Label()
+        Me.Label11 = New System.Windows.Forms.Label()
+        Me.name_p2_red = New System.Windows.Forms.TextBox()
+        Me.Label12 = New System.Windows.Forms.Label()
+        Me.num_p1_red = New System.Windows.Forms.TextBox()
+        Me.Label13 = New System.Windows.Forms.Label()
+        Me.Label14 = New System.Windows.Forms.Label()
+        Me.name_p1_red = New System.Windows.Forms.TextBox()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.GroupBox2 = New System.Windows.Forms.GroupBox()
+        Me.btn_generate = New System.Windows.Forms.Button()
+        Me.outputt = New System.Windows.Forms.Label()
+        Me.GroupBox1.SuspendLayout()
+        Me.GroupBox2.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'name_p1_blue
+        '
+        Me.name_p1_blue.Location = New System.Drawing.Point(60, 57)
+        Me.name_p1_blue.Name = "name_p1_blue"
+        Me.name_p1_blue.Size = New System.Drawing.Size(104, 20)
+        Me.name_p1_blue.TabIndex = 0
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(58, 27)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(45, 13)
+        Me.Label3.TabIndex = 3
+        Me.Label3.Text = "Player 1"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(10, 64)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(35, 13)
+        Me.Label4.TabIndex = 4
+        Me.Label4.Text = "Name"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(1, 97)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(44, 13)
+        Me.Label5.TabIndex = 6
+        Me.Label5.Text = "Number"
+        '
+        'num_p1_blue
+        '
+        Me.num_p1_blue.Location = New System.Drawing.Point(61, 91)
+        Me.num_p1_blue.Name = "num_p1_blue"
+        Me.num_p1_blue.Size = New System.Drawing.Size(105, 20)
+        Me.num_p1_blue.TabIndex = 5
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(0, 200)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(44, 13)
+        Me.Label6.TabIndex = 11
+        Me.Label6.Text = "Number"
+        '
+        'num_p2_blue
+        '
+        Me.num_p2_blue.Location = New System.Drawing.Point(55, 200)
+        Me.num_p2_blue.Name = "num_p2_blue"
+        Me.num_p2_blue.Size = New System.Drawing.Size(105, 20)
+        Me.num_p2_blue.TabIndex = 10
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Location = New System.Drawing.Point(8, 167)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(35, 13)
+        Me.Label7.TabIndex = 9
+        Me.Label7.Text = "Name"
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Location = New System.Drawing.Point(58, 128)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(45, 13)
+        Me.Label8.TabIndex = 8
+        Me.Label8.Text = "Player 2"
+        '
+        'name_p2_blue
+        '
+        Me.name_p2_blue.Location = New System.Drawing.Point(57, 164)
+        Me.name_p2_blue.Name = "name_p2_blue"
+        Me.name_p2_blue.Size = New System.Drawing.Size(104, 20)
+        Me.name_p2_blue.TabIndex = 7
+        '
+        'Label9
+        '
+        Me.Label9.AutoSize = True
+        Me.Label9.Location = New System.Drawing.Point(-3, 194)
+        Me.Label9.Name = "Label9"
+        Me.Label9.Size = New System.Drawing.Size(44, 13)
+        Me.Label9.TabIndex = 21
+        Me.Label9.Text = "Number"
+        '
+        'num_p2_red
+        '
+        Me.num_p2_red.Location = New System.Drawing.Point(47, 194)
+        Me.num_p2_red.Name = "num_p2_red"
+        Me.num_p2_red.Size = New System.Drawing.Size(105, 20)
+        Me.num_p2_red.TabIndex = 20
+        '
+        'Label10
+        '
+        Me.Label10.AutoSize = True
+        Me.Label10.Location = New System.Drawing.Point(4, 161)
+        Me.Label10.Name = "Label10"
+        Me.Label10.Size = New System.Drawing.Size(35, 13)
+        Me.Label10.TabIndex = 19
+        Me.Label10.Text = "Name"
+        '
+        'Label11
+        '
+        Me.Label11.AutoSize = True
+        Me.Label11.Location = New System.Drawing.Point(78, 120)
+        Me.Label11.Name = "Label11"
+        Me.Label11.Size = New System.Drawing.Size(45, 13)
+        Me.Label11.TabIndex = 18
+        Me.Label11.Text = "Player 2"
+        '
+        'name_p2_red
+        '
+        Me.name_p2_red.Location = New System.Drawing.Point(48, 158)
+        Me.name_p2_red.Name = "name_p2_red"
+        Me.name_p2_red.Size = New System.Drawing.Size(104, 20)
+        Me.name_p2_red.TabIndex = 17
+        '
+        'Label12
+        '
+        Me.Label12.AutoSize = True
+        Me.Label12.Location = New System.Drawing.Point(-3, 91)
+        Me.Label12.Name = "Label12"
+        Me.Label12.Size = New System.Drawing.Size(44, 13)
+        Me.Label12.TabIndex = 16
+        Me.Label12.Text = "Number"
+        '
+        'num_p1_red
+        '
+        Me.num_p1_red.Location = New System.Drawing.Point(47, 91)
+        Me.num_p1_red.Name = "num_p1_red"
+        Me.num_p1_red.Size = New System.Drawing.Size(105, 20)
+        Me.num_p1_red.TabIndex = 15
+        '
+        'Label13
+        '
+        Me.Label13.AutoSize = True
+        Me.Label13.Location = New System.Drawing.Point(4, 58)
+        Me.Label13.Name = "Label13"
+        Me.Label13.Size = New System.Drawing.Size(35, 13)
+        Me.Label13.TabIndex = 14
+        Me.Label13.Text = "Name"
+        '
+        'Label14
+        '
+        Me.Label14.AutoSize = True
+        Me.Label14.Location = New System.Drawing.Point(78, 27)
+        Me.Label14.Name = "Label14"
+        Me.Label14.Size = New System.Drawing.Size(45, 13)
+        Me.Label14.TabIndex = 13
+        Me.Label14.Text = "Player 1"
+        '
+        'name_p1_red
+        '
+        Me.name_p1_red.Location = New System.Drawing.Point(48, 55)
+        Me.name_p1_red.Name = "name_p1_red"
+        Me.name_p1_red.Size = New System.Drawing.Size(104, 20)
+        Me.name_p1_red.TabIndex = 12
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.name_p2_blue)
+        Me.GroupBox1.Controls.Add(Me.name_p1_blue)
+        Me.GroupBox1.Controls.Add(Me.Label3)
+        Me.GroupBox1.Controls.Add(Me.Label4)
+        Me.GroupBox1.Controls.Add(Me.num_p1_blue)
+        Me.GroupBox1.Controls.Add(Me.Label5)
+        Me.GroupBox1.Controls.Add(Me.Label8)
+        Me.GroupBox1.Controls.Add(Me.Label7)
+        Me.GroupBox1.Controls.Add(Me.num_p2_blue)
+        Me.GroupBox1.Controls.Add(Me.Label6)
+        Me.GroupBox1.Location = New System.Drawing.Point(12, 33)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(191, 233)
+        Me.GroupBox1.TabIndex = 22
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Team Blues"
+        '
+        'GroupBox2
+        '
+        Me.GroupBox2.Controls.Add(Me.Label14)
+        Me.GroupBox2.Controls.Add(Me.name_p1_red)
+        Me.GroupBox2.Controls.Add(Me.Label9)
+        Me.GroupBox2.Controls.Add(Me.Label13)
+        Me.GroupBox2.Controls.Add(Me.num_p2_red)
+        Me.GroupBox2.Controls.Add(Me.num_p1_red)
+        Me.GroupBox2.Controls.Add(Me.Label10)
+        Me.GroupBox2.Controls.Add(Me.Label12)
+        Me.GroupBox2.Controls.Add(Me.Label11)
+        Me.GroupBox2.Controls.Add(Me.name_p2_red)
+        Me.GroupBox2.Location = New System.Drawing.Point(227, 33)
+        Me.GroupBox2.Name = "GroupBox2"
+        Me.GroupBox2.Size = New System.Drawing.Size(201, 233)
+        Me.GroupBox2.TabIndex = 23
+        Me.GroupBox2.TabStop = False
+        Me.GroupBox2.Text = "Team Reds"
+        '
+        'btn_generate
+        '
+        Me.btn_generate.BackColor = System.Drawing.SystemColors.MenuHighlight
+        Me.btn_generate.ForeColor = System.Drawing.SystemColors.ButtonFace
+        Me.btn_generate.Location = New System.Drawing.Point(107, 285)
+        Me.btn_generate.Name = "btn_generate"
+        Me.btn_generate.Size = New System.Drawing.Size(208, 37)
+        Me.btn_generate.TabIndex = 24
+        Me.btn_generate.Text = "Click to Generate Play by Play Game"
+        Me.btn_generate.UseVisualStyleBackColor = False
+        '
+        'outputt
+        '
+        Me.outputt.AutoSize = True
+        Me.outputt.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.outputt.Location = New System.Drawing.Point(28, 338)
+        Me.outputt.Name = "outputt"
+        Me.outputt.Size = New System.Drawing.Size(0, 24)
+        Me.outputt.TabIndex = 25
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(444, 415)
+        Me.Controls.Add(Me.outputt)
+        Me.Controls.Add(Me.btn_generate)
+        Me.Controls.Add(Me.GroupBox2)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.GroupBox2.ResumeLayout(False)
+        Me.GroupBox2.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents name_p1_blue As TextBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents num_p1_blue As TextBox
+    Friend WithEvents Label6 As Label
+    Friend WithEvents num_p2_blue As TextBox
+    Friend WithEvents Label7 As Label
+    Friend WithEvents Label8 As Label
+    Friend WithEvents name_p2_blue As TextBox
+    Friend WithEvents Label9 As Label
+    Friend WithEvents num_p2_red As TextBox
+    Friend WithEvents Label10 As Label
+    Friend WithEvents Label11 As Label
+    Friend WithEvents name_p2_red As TextBox
+    Friend WithEvents Label12 As Label
+    Friend WithEvents num_p1_red As TextBox
+    Friend WithEvents Label13 As Label
+    Friend WithEvents Label14 As Label
+    Friend WithEvents name_p1_red As TextBox
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents GroupBox2 As GroupBox
+    Friend WithEvents btn_generate As Button
+    Friend WithEvents outputt As Label
+End Class
---
+++ Form1.vb
@@ -0,0 +1,72 @@
+﻿Public Class Form1
+
+
+    Private Sub btn_generate_Click(sender As Object, e As EventArgs) Handles btn_generate.Click
+        Dim playerIndex As Integer = CInt(Math.Floor(Rnd() * 4))
+        Dim action As Integer = CInt(Math.Floor(Rnd() * 3))
+
+
+        Dim var_name_p1_blue As String
+        Dim var_num_p1_blue As Integer
+        Dim var_name_p2_blue As String
+        Dim var_num_p2_blue As Integer
+
+        Dim var_name_p1_red As String
+        Dim var_num_p1_red As Integer
+        Dim var_name_p2_red As String
+        Dim var_num_p2_red As Integer
+
+        var_name_p1_blue = name_p1_blue.Text
+        var_num_p1_blue = num_p1_blue.Text
+        var_name_p2_blue = name_p2_blue.Text
+        var_num_p2_blue = num_p2_blue.Text
+
+        var_name_p1_red = name_p1_red.Text
+        var_num_p1_red = num_p1_red.Text
+        var_name_p2_red = name_p2_red.Text
+        var_num_p2_red = num_p2_red.Text
+
+
+        Dim actionString As String = ""
+        Dim player_wht_name As String = ""
+        Dim player_wht_num As String = ""
+        Dim player_wht_team As String = ""
+
+
+        Select Case action
+            Case 0
+                actionString = "dribbles"
+            Case 1
+                actionString = "passes"
+            Case 2
+                actionString = "shoots"
+        End Select
+
+
+
+        Select Case playerIndex
+            Case 0
+                player_wht_name = var_name_p1_blue
+                player_wht_num = var_num_p1_blue
+                player_wht_team = "Blues"
+
+
+            Case 1
+                player_wht_name = var_name_p2_blue
+                player_wht_num = var_num_p2_blue
+                player_wht_team = "Blues"
+            Case 2
+                player_wht_name = var_name_p1_red
+                player_wht_num = var_num_p1_red
+                player_wht_team = "Reds"
+            Case 3
+                player_wht_name = var_name_p2_red
+                player_wht_num = var_num_p2_red
+                player_wht_team = "Reds"
+        End Select
+        outputt.Text = player_wht_name & ", number " & player_wht_num & " of the " & player_wht_team & " " & actionString & " the ball!"
+
+
+
+    End Sub
+End Class
